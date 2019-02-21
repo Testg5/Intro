79d70241960131e9efffc079e94567fda5b9d79f
Vidya <Vidya@Vidya-VAIO>
2019-02-21 23:13:45 +0530

    First intro prog java

---
+++ src/Intro/Hello.java
@@ -0,0 +1,20 @@
+package Intro;
+
+class Hello
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+		//call();
+	}
+	//StackOverflowError
+	/*final static void call()
+	{
+		System.out.println("Nope");
+		String s="hello hi hru";
+		String[] arr=s.split(" ");
+		main(arr);
+	}*/
+}
+
+   
---
+++ src/Intro/Switchfun.java
@@ -0,0 +1,18 @@
+package Intro;
+
+public class Switchfun 
+{
+	public static void main(String[] args) 
+	{
+		int no=1;
+		
+		switch(no)
+		{
+		case 1:System.out.println("Im one");
+		case 2:System.out.println("Im two");
+		break;
+		case 3:System.out.println("Im three");
+		default:System.out.println("I'm default");
+		}
+	}
+}
